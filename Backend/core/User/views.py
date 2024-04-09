ca33b3c6e197dc4adef962887535eb8f386fd5f8
Arfan Abid <iffionex111@gmail.com>
2024-04-09 16:38:18 +0500

    ~ JWT Authentication

--- Backend/core/User/views.py
+++ Backend/core/User/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import *
 from django.contrib.auth import authenticate,login,logout
-
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserRegister(APIView):
     """
@@ -29,7 +29,8 @@ class UserRegister(APIView):
             return Response({'message':str(e)}, status=status.HTTP_400_BAD_REQUEST)    
 
 
-
+# using JWT (JSON Web Tokens) for authentication can help eliminate CSRF (Cross-Site Request Forgery) issues because JWT authentication does not rely on server-side sessions or cookies to maintain user authentication state.
+# if we don't include JWT TOKEN it will show an error CSRF token is not provoded in Login 
 class UserLogin(APIView):
     """
     API endpoint that allows users to login.
@@ -43,8 +44,14 @@ class UserLogin(APIView):
                 password=serializer.validated_data['password']
                 user=authenticate(username=username, password=password)
                 if user is not None:
+
                     login(request, user)
-                    return Response({'message':'Successfully logged in'}, status=status.HTTP_200_OK)
+                    refresh = RefreshToken.for_user(user)
+                    return Response({
+                        'message': 'Successfully logged in',
+                        'refresh': str(refresh),
+                        'access': str(refresh.access_token)
+                    }, status=status.HTTP_200_OK)
                 else:
                     return Response({'message':'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
             
